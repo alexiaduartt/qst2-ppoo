3ec8082220a8dbab0ac74d908b0770cfa957c9ed
Alexia Alves <lexajosielly@gmail.com>
2025-10-07 17:03:24 -0300

    configurando o cliente Screen e finalizando a solução Observer

--- telephone/Main.java
+++ telephone/Main.java
@@ -1,14 +1,17 @@
-
 public class Main {
     public static void main(String[] args) {
         final int NUM_DIGITS = 12;
 
         // Build the object graph
         PhoneModel model = new PhoneModel();
+        
+        // A Screen recebe o modelo no construtor para registrar os observadores
         Screen screen = new Screen(model);
+        
         KeyPad keyPad = new KeyPad(model);
 
         // Run the program
+        // O KeyPad simula as 12 pressões, o que dispara 12 notificações
         keyPad.simulateKeyPresses(NUM_DIGITS);
     }
 }
--- telephone/Screen.java
+++ telephone/Screen.java
@@ -1,12 +1,26 @@
 /**
  * Prints things out to the screen, when needed
  * Printing to the screen:
- *  System.out.println("hello");
+ * System.out.println("hello");
  */
 public class Screen {
-    private final PhoneModel model;
 
     public Screen(PhoneModel model) {
-        this.model = model;
+        // A Screen cria e registra os observadores no modelo
+        
+        // Observer 1: Imprime o dígito recente
+        model.addObserver(new DigitDisplayObserver(this));
+        
+        // Observer 2: Imprime a mensagem de discagem
+        model.addObserver(new DialingObserver(this));
+    }
+
+    // Métodos de impressão (somente a UI pode imprimir)
+    public void printDigit(int digit) {
+        System.out.println(digit);
+    }
+
+    public void printDialing(String number) {
+        System.out.println("Agora discando " + number + "...");
     }
 }
